39c7cbd58baaa14d6f471ec8b1753a059f02b11f
Hoàng Minh Đức <71325886+hoangminhduc0308@users.noreply.github.com>
2024-02-03 14:43:50 -0800

    Create Exercise6.cs

---
+++ Exercise6.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void EnterMetric(out double met)
+        {
+            Console.Write("Enter amount of meters to convert: ");
+            met = Convert.ToDouble(Console.ReadLine());
+        }
+
+        static void ConvertToFeetAndInches(ref double meters, out double feet, out double inches)
+        {
+            //1m = 100cm
+            //1ft = 30.48cm
+            //number of feet = number of meters * 100 / 30.48
+            feet = (int)(meters * 100 / 30.48);
+            inches = (100.0 * meters - 30.24 * feet) / 2.54;
+        }
+
+        static void PrintFeetAndInches(ref double feet, ref double inches)
+        {
+            Console.WriteLine($"Number of feet: {feet}");
+            Console.WriteLine($"Number of inches: {inches}");
+        }
+
+        static void Main(string[] args) 
+        {
+            double met, feet, inches;
+            EnterMetric(out met);
+            ConvertToFeetAndInches(ref met, out feet, out inches);
+            PrintFeetAndInches(ref feet, ref inches);
+            Console.ReadKey();
+        }
+    }
+}
