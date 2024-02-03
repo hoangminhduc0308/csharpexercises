b4941ca27925c23afe3b3d616e035a383b5b9bc4
Hoàng Minh Đức <71325886+hoangminhduc0308@users.noreply.github.com>
2024-02-03 14:51:38 -0800

    Create Exercise7.cs

---
+++ Exercise7.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+            double charge, tax = 0.09, tip15 = 0.15, tip20 = 0.2;
+            Console.Write("Enter charge (before taxes): ");
+            charge = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Tax: ${(charge * tax):f2}");
+            charge = charge * (1 + tax);
+            
+            Console.WriteLine($"Tip of 15%: ${(charge * tip15):f2}");
+            Console.WriteLine($"Total (with 15% tip): ${(charge * (1 + tip15)):f2}");
+
+            Console.WriteLine($"Tip of 20%: ${(charge * tip20):f2}");
+            Console.WriteLine($"Total (with 20% tip): ${(charge * (1 + tip20)):f2}");
+
+            Console.ReadKey();
+        }
+    }
+}
