1772df1f296961701c3e5cc51c5cda99b97f3159
Hoàng Minh Đức <71325886+hoangminhduc0308@users.noreply.github.com>
2024-02-03 14:53:11 -0800

    Update Exercise7.cs

--- Exercise7.cs
+++ Exercise7.cs
@@ -14,13 +14,13 @@ namespace ConsoleApp2
             double charge, tax = 0.09, tip15 = 0.15, tip20 = 0.2;
             Console.Write("Enter charge (before taxes): ");
             charge = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine($"Tax: ${(charge * tax):f2}");
+            Console.WriteLine($"Tax:                  ${(charge * tax):f2}");
             charge = charge * (1 + tax);
             
-            Console.WriteLine($"Tip of 15%: ${(charge * tip15):f2}");
+            Console.WriteLine($"Tip of 15%:           ${(charge * tip15):f2}");
             Console.WriteLine($"Total (with 15% tip): ${(charge * (1 + tip15)):f2}");
 
-            Console.WriteLine($"Tip of 20%: ${(charge * tip20):f2}");
+            Console.WriteLine($"Tip of 20%:           ${(charge * tip20):f2}");
             Console.WriteLine($"Total (with 20% tip): ${(charge * (1 + tip20)):f2}");
 
             Console.ReadKey();
